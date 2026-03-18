c297e304c06c2d9ad2fde57c38c1e60508648666
Sergey <sergeyh510@gmail.com>
2026-03-18 12:15:01 +0300

    Закомментированна строка import org.springframework.kafka.support.serializer.JacksonJsonSerializer; // Новый сериализатор Так как было задвоение

--- src/main/java/com/example/jokekafkaproducer/JokeKafkaProducerApplication.java
+++ src/main/java/com/example/jokekafkaproducer/JokeKafkaProducerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class JokeKafkaProducerApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(JokeKafkaProducerApplication.class, args);
     }
 }
--- src/main/java/com/example/jokekafkaproducer/config/KafkaConfig.java
+++ src/main/java/com/example/jokekafkaproducer/config/KafkaConfig.java
@@ -52,21 +52,3 @@ public class KafkaConfig {
         return new KafkaTemplate<>(producerFactory());
     }
 }
-
-//package com.example.jokekafkaproducer.config;
-
-//import org.apache.kafka.clients.admin.NewTopic;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.kafka.config.TopicBuilder;
-
-//@Configuration
-//public class KafkaConfig {
-//    @Bean
-//    public NewTopic jokesTopic() {
-//        return TopicBuilder.name("jokes-topic")
-//                .partitions(1)
-//                .replicas(1)
-//                .build();
-//    }
-//}
